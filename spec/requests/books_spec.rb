9e88d5a6aa4cb655654d16d5dc8ec7b8e3b96b70
Excavator <kvronin@gmail.com>
2023-04-17 23:55:12 +0600

    add authorization for user

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe "Books", type: :request do
   let!(:books) { create_list(:book, 5) }
   let(:book_id) { books.first.id }
+  let(:user) { create(:user) }
+
   describe "GET /books" do
-    before { get '/api/v1/books' }
+    before { get '/api/v1/books', headers: { 'Authorization' => AuthenticationTokenService.call(user.id) } }
     it "returns books" do
       expect(json).not_to be_empty
       expect(json.size).to eq(5)
@@ -15,7 +17,7 @@ RSpec.describe "Books", type: :request do
   end
 
   describe "GET /books/:id" do
-    before { get "/api/v1/books/#{book_id}" }
+    before { get "/api/v1/books/#{book_id}", headers: { 'Authorization' => AuthenticationTokenService.call(user.id) } }
     context "when book exists" do
       it "returns status code 200" do
         expect(response).to have_http_status(200)
@@ -37,20 +39,19 @@ RSpec.describe "Books", type: :request do
 
   describe "POST /books/:id" do
     let!(:fancy) { create(:category) }
-    let!(:user) { create(:user) }
     let(:valid_attributes) do
       { title: 'The Hobbit', author: 'J.R.R. Tolkien',
         category_id: fancy.id, user_id: user.id }
     end
 
     context "when request attributes are valid" do
-      before { post '/api/v1/books', params: valid_attributes }
+      before { post '/api/v1/books', params: valid_attributes, headers: { 'Authorization' => AuthenticationTokenService.call(user.id) } }
       it "returns status code 201" do
         expect(response).to have_http_status(201)
       end
     end
     context "when an invalid request" do
-      before { post '/api/v1/books', params: {} }
+      before { post '/api/v1/books', params: {}, headers: { 'Authorization' => AuthenticationTokenService.call(user.id) } }
       it "returns starus code 422" do
         expect(response).to have_http_status(422)
       end
@@ -62,7 +63,7 @@ RSpec.describe "Books", type: :request do
 
   describe "PUT /books/:id" do
     let(:valid_attributes) { { title: 'KVexcavator' } }
-    before { put "/api/v1/books/#{book_id}", params: valid_attributes }
+    before { put "/api/v1/books/#{book_id}", params: valid_attributes, headers: { 'Authorization' => AuthenticationTokenService.call(user.id) } }
     context "when book exists" do
       it "returns status code 204" do
         expect(response).to have_http_status(204)
@@ -84,7 +85,7 @@ RSpec.describe "Books", type: :request do
   end
 
   describe "DELETE /books/:id" do
-    before { delete "/api/v1/books/#{book_id}" }
+    before { delete "/api/v1/books/#{book_id}", headers: { 'Authorization' => AuthenticationTokenService.call(user.id) } }
     it "returns status code 204" do
       expect(response).to have_http_status(204)
     end
